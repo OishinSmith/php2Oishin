8ded90faf3c631121d0673e19b9250d5ab62c742
vagrant <vagrant@php-training>
2023-05-05 04:19:31 -0700

    fixing git issues

---
+++ createClass.php
@@ -0,0 +1,43 @@
+<?php
+//Complete the following:
+//1. Create a class definition that represents or models something. Give it a constant, some properties, and a few methods. Set appropriate visibilities for each.
+//2. Instantiate a couple of objects, and execute the methods created producing some output.
+//3. Create something which is realistic and appropriate to a current or future application for your domain.
+//Lab complete
+
+namespace lab1;
+
+class Customer {
+
+	private $name;
+	private $age;
+	private $appartmentNumber;
+	
+	public function __construct(string $name, int $age, ?int $appartmentNumber) {
+		$this->name = $name;
+		$this->age = $age;
+		$this->appartmentNumber = $appartmentNumber;
+	}
+
+	// get the room that a guest is renting out
+	public function getAppartmentNumber(){
+		return $this->appartmentNumber ?? null;
+	}
+	
+	// rent the room to a person m
+	public function rentAppartment(int $appartmentNumber){
+		$this->appartmentNumber = $appartmentNumber;
+		echo 'appartmentNumber ' . $this->appartmentNumber . ' has been set';
+	}
+	
+	// check if room is available
+	public function isAppartmentAvailable(int $appartmentNumber){
+		$roomAvailable = checkIfAppartmentAvailable($appartmentNumber);
+		return $roomAvailable;
+	}
+	
+	public function checkIfAppartmentAvailable(int $appartmentNumber){
+		// check database if room is available
+		return true;
+	}
+}
